b04f78afb155ce3a9843284aa2e9aacec47b88f0
Lupino <lmjubuntu@gmail.com>
2017-08-28 17:45:11 +0800

    update

--- smart_config_dht.ino
+++ smart_config_dht.ino
@@ -3,13 +3,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <EEPROM.h>
-
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include "DHT.h"
-
-// #define WIFI_AP "WX_Urwork_2.4G"
-// #define WIFI_PASSWORD "urwork2015"
-
-#define TOKEN "GQ7Pn6Vd5SGGjtXGJOA4"
 
 #define SMART_CONFIG_BUTTON 14
 #define SMART_CONFIG_LED 2
@@ -21,7 +17,7 @@
 // Initialize DHT sensor.
 DHT dht(DHT_PIN, DHT_TYPE);
 
-bool led_status = false;
+bool blinkStatus = false;
 
 char thingsboardServer[] = "tb.codecard.cn";
 
@@ -29,17 +25,16 @@ WiFiClient wifiClient;
 
 PubSubClient client(wifiClient);
 
+ESP8266WebServer server(80);
+
 unsigned long lastSend;
 unsigned long periodic = 15000;
 
-char wifi_ap[32];
-char wifi_password[64];
+char wifiAP[32];
+char wifiPassword[64];
+char token[30];
 
 void setup() {
-  // Serial.begin(115200);
-  // Set output mode for all GPIO pins
-  // while (!Serial) {};
-
   EEPROM.begin(512);
   pinMode(DHT_VCC, OUTPUT);
   digitalWrite(DHT_VCC, LOW);
@@ -49,40 +44,38 @@ void setup() {
   delay(10);
 
   for (int i = 0; i < 32; ++i) {
-    wifi_ap[i] = char(EEPROM.read(i));
+    wifiAP[i] = char(EEPROM.read(i));
   }
 
   for (int i = 32; i < 96; ++i) {
-    wifi_password[i - 32] = char(EEPROM.read(i));
+    wifiPassword[i - 32] = char(EEPROM.read(i));
+  }
+
+  for (int i = 96; i < 126; ++i) {
+    token[i - 96] = char(EEPROM.read(i));
   }
 
   InitWiFi();
   client.setServer( thingsboardServer, 1883 );
-  client.setCallback(on_message);
-  lastSend = 0;
+  client.setCallback(onMessage);
+
+  server.on("/update_token", HTTP_POST, handleSetToken);
+  server.onNotFound(handleNotFound);
+  server.begin();
 }
 
 // The callback for when a PUBLISH message is received from the server.
-void on_message(const char* topic, byte* payload, unsigned int length) {
-
-  // Serial.println("On message");
+void onMessage(const char* topic, byte* payload, unsigned int length) {
 
   char json[length + 1];
-  strncpy (json, (char*)payload, length);
+  strncpy(json, (char*)payload, length);
   json[length] = '\0';
-
-  // Serial.print("Topic: ");
-  // Serial.println(topic);
-  // Serial.print("Message: ");
-  // Serial.println(json);
 
   // Decode JSON request
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& data = jsonBuffer.parseObject((char*)json);
 
-  if (!data.success())
-  {
-    // Serial.println("parseObject() failed");
+  if (!data.success()) {
     return;
   }
 
@@ -93,18 +86,18 @@ void on_message(const char* topic, byte* payload, unsigned int length) {
     // Reply with GPIO status
     String responseTopic = String(topic);
     responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), get_periodic().c_str());
+    client.publish(responseTopic.c_str(), getPeriodic().c_str());
   } else if (methodName.equals("setPeriodic")) {
     // Update GPIO status and reply
-    set_periodic(data["params"]["periodic"]);
+    setPeriodic(data["params"]["periodic"]);
     String responseTopic = String(topic);
     responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), get_periodic().c_str());
-    client.publish("v1/devices/me/attributes", get_periodic().c_str());
+    client.publish(responseTopic.c_str(), getPeriodic().c_str());
+    client.publish("v1/devices/me/attributes", getPeriodic().c_str());
   }
 }
 
-String get_periodic() {
+String getPeriodic() {
   // Prepare gpios JSON payload string
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& data = jsonBuffer.createObject();
@@ -117,14 +110,25 @@ String get_periodic() {
   return strPayload;
 }
 
-void set_periodic(unsigned long periodic_) {
+void setPeriodic(unsigned long periodic_) {
     if (periodic_ < 2000) {
         periodic_ = 2000;
     }
     periodic = periodic_;
 }
 
-void smart_config() {
+String getLocalIP() {
+  // Prepare relays JSON payload string
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& data = jsonBuffer.createObject();
+  data["ip"] = WiFi.localIP().toString();
+  char payload[256];
+  data.printTo(payload, sizeof(payload));
+  String strPayload = String(payload);
+  return strPayload;
+}
+
+void smartConfig() {
   if (digitalRead(SMART_CONFIG_BUTTON)) {
     return;
   }
@@ -135,47 +139,54 @@ void smart_config() {
   }
   WiFi.disconnect();
 
-  bool led_status = true;
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
-    smart_blink();
+    smartBlink();
     WiFi.beginSmartConfig();
     while(1){
       delay(200);
-      smart_blink();
+      smartBlink();
       if (WiFi.smartConfigDone()) {
         break;
       }
     }
   }
 
-  digitalWrite(SMART_CONFIG_LED, HIGH);
+  closeBlink();
 
-  strcpy(wifi_ap, WiFi.SSID().c_str());
-  strcpy(wifi_password, WiFi.psk().c_str());
+  strcpy(wifiAP, WiFi.SSID().c_str());
+  strcpy(wifiPassword, WiFi.psk().c_str());
 
   for (int i = 0; i < 32; ++i) {
-    EEPROM.write(i, wifi_ap[i]);
+    EEPROM.write(i, wifiAP[i]);
   }
 
   for (int i = 32; i < 96; ++i) {
-    EEPROM.write(i, wifi_password[i - 32]);
+    EEPROM.write(i, wifiPassword[i - 32]);
   }
   EEPROM.commit();
 }
 
-void smart_blink() {
-  if (led_status) {
-    digitalWrite(SMART_CONFIG_LED, HIGH);
-    led_status = false;
+void smartBlink() {
+  if (blinkStatus) {
+    closeBlink();
   } else {
-    digitalWrite(SMART_CONFIG_LED, LOW);
-    led_status = true;
+    openBlink();
   }
 }
 
+void openBlink() {
+  digitalWrite(SMART_CONFIG_LED, LOW);
+  blinkStatus = true;
+}
+
+void closeBlink() {
+  digitalWrite(SMART_CONFIG_LED, HIGH);
+  blinkStatus = false;
+}
+
 void loop() {
-  if ( !client.connected() ) {
+  if (!client.connected()) {
     reconnect();
   }
 
@@ -188,14 +199,13 @@ void loop() {
     lastSend = millis();
   }
 
-  smart_config();
-
   client.loop();
+
+  smartConfig();
+  server.handleClient();
 }
 
 void getAndSendTemperatureAndHumidityData() {
-  // Serial.println("Collecting temperature data.");
-
   // Reading temperature or humidity takes about 250 milliseconds!
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
@@ -203,85 +213,86 @@ void getAndSendTemperatureAndHumidityData() {
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
-    // Serial.println("Failed to read from DHT sensor!");
     return;
   }
-
-  // Serial.print("Humidity: ");
-  // Serial.print(h);
-  // Serial.print(" %\t");
-  // Serial.print("Temperature: ");
-  // Serial.print(t);
-  // Serial.print(" *C ");
 
   String temperature = String(t);
   String humidity = String(h);
 
-  auto vcc = ESP.getVcc();
-
-
-  // Just debug messages
-  // Serial.print( "Sending temperature and humidity : [" );
-  // Serial.print( temperature ); Serial.print( "," );
-  // Serial.print( humidity );
-  // Serial.print( "]   -> " );
-
   // Prepare a JSON payload string
   String payload = "{";
   payload += "\"temperature\":"; payload += temperature; payload += ",";
-  payload += "\"humidity\":"; payload += humidity; payload += ",";
-  payload += "\"vcc\":"; payload += String(vcc);
+  payload += "\"humidity\":"; payload += humidity;
   payload += "}";
 
   // Send payload
   char attributes[100];
-  payload.toCharArray( attributes, 100 );
-  client.publish( "v1/devices/me/telemetry", attributes );
-  // Serial.println( attributes );
+  payload.toCharArray(attributes, 100);
+  client.publish("v1/devices/me/telemetry", attributes);
 }
 
 void InitWiFi() {
-  // Serial.println("Connecting to AP ...");
   // attempt to connect to WiFi network
-
   // set for STA mode
   WiFi.mode(WIFI_STA);
 
-  WiFi.begin(wifi_ap, wifi_password);
+  WiFi.begin(wifiAP, wifiPassword);
+  configWiFiWithSmartConfig();
+}
+
+void configWiFiWithSmartConfig() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    // Serial.print(".");
-    smart_blink();
-    smart_config();
+    smartBlink();
+    smartConfig();
   }
-  // Serial.println("Connected to AP");
+  closeBlink();
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    if ( WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(wifi_ap, wifi_password);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        smart_blink();
-        // Serial.print(".");
-        smart_config();
-      }
-      // Serial.println("Connected to AP");
+    if (WiFi.status() != WL_CONNECTED) {
+      configWiFiWithSmartConfig();
     }
-    // Serial.print("Connecting to Thingsboard node ...");
     // Attempt to connect (clientId, username, password)
-    if ( client.connect("ESP8266 Device", TOKEN, NULL) ) {
-      // Serial.println( "[DONE]" );
+    if (client.connect("ESP8266 Relay", token, NULL)) {
       client.subscribe("v1/devices/me/rpc/request/+");
-      client.publish("v1/devices/me/attributes", get_periodic().c_str());
+      client.publish("v1/devices/me/attributes", getPeriodic().c_str());
+      client.publish("v1/devices/me/attributes", getLocalIP().c_str());
     } else {
-      // Serial.print( "[FAILED] [ rc = " );
-      // Serial.print( client.state() );
-      // Serial.println( " : retrying in 5 seconds]" );
       // Wait 5 seconds before retrying
-      delay( 5000 );
+
+      unsigned long lastCheck = millis();
+      while (millis() - lastCheck < 5000) {
+        smartConfig();
+        server.handleClient();
+      }
+      smartBlink();
     }
   }
+  closeBlink();
+}
+
+void handleSetToken() {
+  boolean updated = false;
+  for (uint8_t i=0; i<server.args(); i++){
+    if (server.argName(i) == "token") {
+        strcpy(token, server.arg(i).c_str());
+        updated = true;
+        break;
+    };
+  }
+  if (updated) {
+    for (int i = 96; i < 126; ++i) {
+      EEPROM.write(i, token[i - 96]);
+    }
+    EEPROM.commit();
+    client.disconnect();
+  }
+  server.send(200, "text/plain", "OK");
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "Not Found");
 }
